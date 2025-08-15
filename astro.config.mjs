429a846385a1967d797df444bab623e79a23c862
Manuel Spigolon <behemoth89@gmail.com>
2025-08-15 11:53:28 +0200

    feat: sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      i18n: {
+        defaultLocale: 'it',
+        locales: {
+          it: 'it',
+          en: 'en'
+        }
+      }
+    })
+  ],
   site: 'https://studiolegaleferrini.com',
   build: {
     format: 'file'
